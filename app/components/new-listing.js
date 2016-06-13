62bbf3c430a696b3c4cf40de5e6b58acd14dd03a
DJ Roychoudhury and Kevin Deganos <kevin@>
2016-06-13 11:41:13 -0700

    add save function, display basic listing information in tile.

---
+++ app/components/new-listing.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewListing: false,
+  actions: {
+    listingFormShow() {
+      this.set('addNewListing', true);
+    },
+    saveListing() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        price: parseInt(this.get('price')),
+        image: this.get('image'),
+        description: this.get('description'),
+        date: this.get('date'),
+        category: this.get('category')
+      };
+      this.set('addNewListing', false);
+      this.sendAction('saveListing', params);
+    }
+  }
+});
--- app/router.js
+++ app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('listing');
+  this.route('listing', {path: 'listing/:listing_id'});
   this.route('category', {path: 'category/:category_id'});
 });
 
